97e8088256f8b94b99af3ac1959e95a989aa8a6f
SSKirillSS <SSTroveSS@Gmail.com>
2024-08-01 20:50:36 +0300

    Extended config example

--- src/main/java/it/hurts/sskirillss/octolib/ConfigInit.java
+++ src/main/java/it/hurts/sskirillss/octolib/ConfigInit.java
@@ -1,9 +1,8 @@
 package it.hurts.sskirillss.octolib;
 
-import it.hurts.sskirillss.octolib.config.annotations.registration.Config;
+import it.hurts.sskirillss.octolib.config.ConfigManager;
 import it.hurts.sskirillss.octolib.config.annotations.registration.ConfigRegistration;
-import it.hurts.sskirillss.octolib.config.cfgbuilder.CompoundEntry;
-import it.hurts.sskirillss.octolib.config.impl.CompoundConfig;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,21 +11,13 @@ import java.util.Map;
 public class ConfigInit {
     public static final Map<Integer, TestConfig> CONFIGS = new HashMap<>();
 
-    @Config("test")
-    public static CompoundConfig CONFIG = new CompoundConfig() {
-        @Override
-        public void write(CompoundEntry compoundEntry) {
-            for (int i = 0; i < 10; i++) {
-                compoundEntry.putObject("entry_" + i, new TestConfig());
-            }
-        }
+    static {
+        for (int i = 0; i < 10; i++) {
+            TestConfig config = new TestConfig();
 
-        @Override
-        public void read(CompoundEntry compoundEntry) {
-            for (int i = 0; i < 10; i++) {
-                CONFIGS.put(i, compoundEntry.getObject("entry_" + i, TestConfig.class));
-            }
+            ConfigManager.registerConfig(ResourceLocation.fromNamespaceAndPath(OctoLib.MODID, "test/entry_" + i), config);
+
+            CONFIGS.put(i, config);
         }
-    
-    };
+    }
 }
--- src/main/java/it/hurts/sskirillss/octolib/TestConfig.java
+++ src/main/java/it/hurts/sskirillss/octolib/TestConfig.java
@@ -1,8 +1,9 @@
 package it.hurts.sskirillss.octolib;
 
+import it.hurts.sskirillss.octolib.config.impl.OctoConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
-public class TestConfig {
+public class TestConfig implements OctoConfig {
     public int meow = 5;
 
     public String honk = "HONK";
@@ -10,4 +11,14 @@ public class TestConfig {
     public double oink = 4.4D;
 
     public Pair<String, String> woof = Pair.of("WOOF_1", "WOOF_2");
+
+    @Override
+    public Object prepareData() {
+        return this;
+    }
+
+    @Override
+    public void onLoadObject(Object object) {
+
+    }
 }

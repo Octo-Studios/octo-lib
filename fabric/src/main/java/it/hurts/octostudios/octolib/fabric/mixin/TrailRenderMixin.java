a0aa311dde5208ab27a0ca47a5a2127f18662724
catboybinary <dev@catbot.page>
2024-12-27 17:49:46 +0300

    1.21.4

--- fabric/src/main/java/it/hurts/octostudios/octolib/fabric/mixin/TrailRenderMixin.java
+++ fabric/src/main/java/it/hurts/octostudios/octolib/fabric/mixin/TrailRenderMixin.java
@@ -1,50 +1,46 @@
 package it.hurts.octostudios.octolib.fabric.mixin;
 
-import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
 import com.mojang.blaze3d.vertex.PoseStack;
 import it.hurts.octostudios.octolib.modules.particles.RenderProvider;
 import it.hurts.octostudios.octolib.modules.particles.OctoRenderManager;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.client.renderer.*;
 import net.minecraft.world.phys.Vec3;
-import org.joml.Matrix4f;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LevelRenderer.class)
+@Mixin(ParticleEngine.class)
 public abstract class TrailRenderMixin {
-    
-    @Shadow
-    @Final
-    private RenderBuffers renderBuffers;
 
-    @Shadow
-    protected abstract void checkPoseStack(PoseStack poseStack);
-    
+    @Unique
+    private void checkPoseStack(PoseStack poseStack) {
+        if (!poseStack.clear()) {
+            throw new IllegalStateException("Pose stack not empty");
+        }
+    }
+
     // net.minecraft.client.renderer.RenderBuffers.bufferSource
     // Lnet/minecraft/client/renderer/RenderBuffers;bufferSource()Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;
-    @Inject(method = "renderLevel",
-            at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/LevelRenderer;addParticlesPass(Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/FogParameters;)V"))
-    public void render(GraphicsResourceAllocator graphicsResourceAllocator, DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
-        render(deltaTracker, bl, camera, gameRenderer, null, matrix4f, matrix4f2);
+    @Inject(method = "render",
+            at = @At(value = "HEAD"))
+    public void render(Camera camera, float f, MultiBufferSource.BufferSource bufferSource, CallbackInfo ci) {
+        render(Minecraft.getInstance().getDeltaTracker(), camera);
     }
 
     @Unique
-    private void render(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, Matrix4f matrix4f2) {
+    private void render(DeltaTracker deltaTracker, Camera camera) {
         Vec3 vec3 = camera.getPosition();
         double d = vec3.x();
         double e = vec3.y();
         double g = vec3.z();
 
-        MultiBufferSource.BufferSource bufferSource = renderBuffers.bufferSource();
+        MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
         PoseStack poseStack = new PoseStack();
         float f = deltaTracker.getGameTimeDeltaPartialTick(false);
 

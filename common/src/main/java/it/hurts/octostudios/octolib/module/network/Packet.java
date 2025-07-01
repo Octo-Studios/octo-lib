9cca02867519eba4234325092c5efb8352d1f907
catboybinary <catboybinary@disroot.org>
2025-07-01 22:22:44 +0300

    remove Env.SERVER bc it's not needed

--- common/src/main/java/it/hurts/octostudios/octolib/module/network/Packet.java
+++ common/src/main/java/it/hurts/octostudios/octolib/module/network/Packet.java
@@ -34,7 +34,6 @@ public abstract class Packet implements CustomPacketPayload {
 
     }
 
-    @Environment(EnvType.SERVER)
     protected void handleServer(NetworkManager.PacketContext packetContext) {
 
     }

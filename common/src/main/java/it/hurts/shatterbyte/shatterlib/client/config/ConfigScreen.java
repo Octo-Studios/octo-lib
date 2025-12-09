b5160aca3ee6fe80ee2198ec33ffedc4ecdd4c7e
binary <catboybinary@disroot.org>
2025-12-09 18:17:52 +0300

    repositioning refactor

--- common/src/main/java/it/hurts/shatterbyte/shatterlib/client/config/ConfigScreen.java
+++ common/src/main/java/it/hurts/shatterbyte/shatterlib/client/config/ConfigScreen.java
@@ -2,9 +2,11 @@ package it.hurts.shatterbyte.shatterlib.client.config;
 
 import dev.architectury.platform.Platform;
 import it.hurts.shatterbyte.shatterlib.ShatterLib;
+import it.hurts.shatterbyte.shatterlib.client.config.widget.DynamicallySized;
 import it.hurts.shatterbyte.shatterlib.client.config.widget.GenericObjectWidget;
 import it.hurts.shatterbyte.shatterlib.module.config.ShatterConfig;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -13,20 +15,29 @@ public class ConfigScreen extends Screen {
     public static final Atlas ATLAS = new Atlas(ResourceLocation.fromNamespaceAndPath(ShatterLib.MOD_ID, "textures/config/config_atlas.png"), 263, 76);
     ShatterConfig config;
     Screen prevScreen;
+    GenericObjectWidget object;
 
     public ConfigScreen(ShatterConfig config, Screen prevScreen) {
         super(Component.literal(config.getName()));
         this.config = config;
         this.prevScreen = prevScreen;
+
+        object = new GenericObjectWidget(config, null, null, () -> config, conf -> {});
+        this.addRenderableWidget(object);
+
+        repositionElements();
     }
 
     @Override
     protected void init() {
         super.init();
-        GenericObjectWidget configWidget = new GenericObjectWidget(config, null, null, () -> config, config -> {});
-        configWidget.setWidth(this.width);
-        configWidget.repositionElements();
-        this.addRenderableWidget(configWidget);
+
+    }
+
+    @Override
+    protected void repositionElements() {
+        object.setWidth(this.width);
+        object.repositionElements();
     }
 
     @Override

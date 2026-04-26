dfffaecafea02415ab1e2855e13222509637544a
binary <catboybinary@disroot.org>
2026-04-26 16:22:52 +0300

    removed architectury doohickey

--- common/src/main/java/it/hurts/shatterbyte/shatterlib/module/config/type/adapter/ItemAdapter.java
+++ common/src/main/java/it/hurts/shatterbyte/shatterlib/module/config/type/adapter/ItemAdapter.java
@@ -5,15 +5,21 @@ import de.marhali.json5.Json5Primitive;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 
 public class ItemAdapter implements TypeAdapter<Item> {
     @Override
     public Json5Element encode(Item value) {
-        return Json5Primitive.fromString(value.arch$registryName().toString());
+        return Json5Primitive.fromString(BuiltInRegistries.ITEM.getKey(value).toString());
     }
 
     @Override
     public Item decode(Json5Element json) {
-        return BuiltInRegistries.ITEM.getValue(Identifier.parse(json.getAsString()));
+        Identifier id = Identifier.parse(json.getAsString());
+        if (!BuiltInRegistries.ITEM.containsKey(id)) {
+            return Items.AIR;
+        }
+
+        return BuiltInRegistries.ITEM.getValue(id);
     }
 }
